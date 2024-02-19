914eae85338b737bbfa79bf5e3ee2c646a9f42fa
Tomkndn <ntamsoy66@gmail.com>
2024-02-20 01:06:09 +0530

    modified readme file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,9 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
   },
 }
